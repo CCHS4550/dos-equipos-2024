a08cba35dc6e631f581fcd8f2b0b538849e2dc23
RuhanRajasthan <144182793+RuhanRajasthan@users.noreply.github.com>
2024-12-01 11:25:48 -0600

    kil me

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -33,7 +33,6 @@ public class RobotContainer {
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    MechanismScheme.configure(door, 0);
   DriveScheme.configure(driveTrain, door,0);
   
     // Configure the trigger bindings
